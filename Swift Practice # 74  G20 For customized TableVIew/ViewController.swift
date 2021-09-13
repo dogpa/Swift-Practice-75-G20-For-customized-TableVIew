75ca98adde926c88ada07482ca98cda6d77b3d21
Dogpa <dogpa0130@gmail.com>
2021-09-13 14:31:37 +0800

    Initial Commit

---
+++ Swift Practice # 74  G20 For customized TableVIew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Practice # 74  G20 For customized TableVIew
+//
+//  Created by Dogpa's MBAir M1 on 2021/9/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
